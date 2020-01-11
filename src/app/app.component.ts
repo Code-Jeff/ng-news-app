6b8b5a099a5feb15d0f6e425a761793c6ed7f85a
Code-Jeff <jeff.code@outlook.com>
2020-01-10 23:08:05 -0600

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,24 @@ import { NewsApiService } from './news-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   mArticles:Array<any>;
   mSources:Array<any>;
 
   constructor(private newsapi:NewsApiService){
-    console.log('app component constructor called');
+    console.log('app component constructor called');         
   }
 
   ngOnInit() {
-      // load articles
-    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
-    // load news sources
-    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
-  }
-  
+        //load articles
+      this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    //load news sources
+    this.newsapi.initSources().subscribe(data=> this.mSources = data['sources']);  
+    }
+
   searchArticles(source){
     console.log("selected source is: "+source);
     this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
   }
-  
+
 }
--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
- 
+import { HttpClient  } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,15 +8,14 @@ export class NewsApiService {
 
   api_key = '8f502064ee554aa19fd0cb6253679597';
 
-
   constructor(private http:HttpClient) { }
   initSources(){
-    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
   }
   initArticles(){
-    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
   }
   getArticlesByID(source: String){
-    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&api_key='+this.api_key);
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
   }
-}
+} 
